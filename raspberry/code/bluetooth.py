8b7708f43c46328cfb36d35be9433acb02d530dc
NicoPiko05 <nicosimoni@seznam.cz>
2025-02-22 18:24:09 +0100

    bluetooth hoefully working

---
+++ raspberry/code/bluetooth.py
@@ -0,0 +1,30 @@
+import os
+import sys
+import time
+import logging
+import threading
+import connection
+
+sys.path.append("home/nico/Documents/")
+from bluepy import btle
+
+class BluetoothService:
+    def __init__(self, name="SmartGlasses"):
+        self.name = name
+        self.advertise_service()
+
+    def advertise_service(self):
+        os.system("sudo hciconfig hci0 up")
+        os.system("sudo hciconfig hci0 piscan")
+        os.system(f"sudo hciconfig hci0 name {self.name}")
+        os.system("sudo sdptool add SP")
+        os.system("sudo rfcomm watch hci0")
+
+    def start(self):
+        print(f"{self.name} is now discoverable and connectable.")
+
+if __name__ == "__main__":
+    bt_service = BluetoothService()
+    bt_service.start()
+    while True:
+        time.sleep(60)
