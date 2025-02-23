ccfca4fa5442aea3acd3ca61ff8b2ae37104c062
NicoPiko05 <nicosimoni@seznam.cz>
2025-02-23 18:02:25 +0100

    pybluez phone

---
+++ Phone/pybluez-0.23/pybluez-0.23/examples/ble/scan.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""PyBluez ble example scan.py"""
+
+from bluetooth.ble import DiscoveryService
+
+service = DiscoveryService()
+devices = service.discover(2)
+
+for address, name in devices.items():
+    print("Name: {}, address: {}".format(name, address))
