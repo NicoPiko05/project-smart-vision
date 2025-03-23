d39ea855c8509d85a85cf84a6a5570154f026eea
NicoPiko05 <nicosimoni@seznam.cz>
2025-03-23 21:54:47 +0100

    FUCKKK

--- raspberry/code/connection.py
+++ raspberry/code/connection.py
@@ -39,7 +39,7 @@ def show_on_display(text):
         draw.text((30, 30), text, fill = "WHITE", font=Font1)
         image1=mirror_image(image1)
         disp.ShowImage(image1)
-        time.sleep(15)
+        #time.sleep(15)
     except IOError as e:
         logging.info(e)
 
@@ -52,48 +52,10 @@ def update_time_on_display():
         # Wait for 60 seconds before updating the time again
         time.sleep(60)
 
-def start_server():
-    try:
-        # Server address and port
-        server_address = '0.0.0.0'  #(localhost)
-        server_port = 65432  # Port number to listen on
-        # Create a socket object
-        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # Bind the socket to the address and port
-        server_socket.bind((server_address, server_port))
-        # Listen for incoming connections (max 1 connection in the queue)
-        server_socket.listen(1)
-        print(f"Server listening on {server_address}:{server_port}")
 
-        # Accept a connection
-        connection, client_address = server_socket.accept()
-        print(f"Connection from {client_address}")
-
-        try:
-            while(1 == 1):
-                # Receive the message from the client
-                message = connection.recv(1024).decode('utf-8')
-                print(f"Message from client: {message}")
-                show_on_display(message)
-
-                # Send a response back to the client
-                response_message = "Hello, Client!"
-                connection.sendall(response_message.encode('utf-8'))
-                print(f"Sent response to client: {response_message}")
-                time.sleep(15)
-
-        finally:
-            # Clean up the connection
-            connection.close()
-            print("Connection closed")
-    except Exception as e:
-        print(f"An error occured: {e}")
 
 show_on_display("Hello, World!")
-time.sleep(5000)
-# Start the server in a separate thread
-server_thread = threading.Thread(target=start_server)
-server_thread.start()
+#time.sleep(5000)
 
 # Start the time update function in a separate thread
 time_thread = threading.Thread(target=update_time_on_display)
