b2eebab4cbe6555b80e1b9fbc5f483dca8857a36
NicoPiko05 <nicosimoni@seznam.cz>
2025-03-26 20:56:38 +0100

    bluetooth test

--- raspberry/code/Main.py
+++ raspberry/code/Main.py
@@ -19,6 +19,7 @@ server_sock.bind(("", bluetooth.PORT_ANY))
 server_sock.listen(1)
 
 port = server_sock.getsockname()[1]
+
 bluetooth.advertise_service(server_sock, "AudioStream",
                             service_classes=[bluetooth.SERIAL_PORT_CLASS],
                             profiles=[bluetooth.SERIAL_PORT_PROFILE])
