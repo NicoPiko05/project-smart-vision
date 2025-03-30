4a261d55b42e154537252880e56ae991a4d09247
NicoPiko05 <nicosimoni@seznam.cz>
2025-03-30 13:06:26 +0200

    Revert "gonna try the phone tmrw"
    This reverts commit 38ff7c63d834744af8bd6e5b1dcda2a032dcdb64.

--- sv_phone/lib/main.dart
+++ sv_phone/lib/main.dart
@@ -1,4 +1,22 @@
 import 'dart:async';
 import 'dart:developer' as dev;
 import 'package:flutter/material.dart';
-import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
+import 'package:sv_phone/pages/home.dart';
+import 'logic/bluetooth.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+
+    );
+
+  }
+}
