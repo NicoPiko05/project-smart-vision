4c57c5041e94b775549b50a4752a20e10b74cf23
NicoPiko05 <144665228+NicoPiko05@users.noreply.github.com>
2025-04-03 15:37:58 +0200

    something

---
+++ sv_phone/lib/logic/raspibluetooth.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
+
+//receives pyaudio stream from raspberry pi and transcribes it using vosk speech recognition
---
+++ sv_phone/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:sv_phone/pages/home.dart';
+
+void main(){
+  runApp(const Homepage());
+}
